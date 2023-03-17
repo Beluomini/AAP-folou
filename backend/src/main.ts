9c95c46014208b8b5c7cff6c987950f9352edc33
Lucas Beluomini <lucasbeluomini1@gmail.com>
2023-03-17 10:53:45 -0300

    resolvendo tratamento da politica cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({origin: '*'} as CorsOptions)
 
   app.useGlobalPipes(new ValidationPipe());
 
