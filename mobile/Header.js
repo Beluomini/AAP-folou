78af6f35fc1ced94f1b5f342c29cc81983296e47
Lucas Beluomini <lucasbeluomini1@gmail.com>
2023-03-22 12:09:19 -0300

    adicionando oo header nas paginas igualmente

---
+++ mobile/Header.js
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import {View, Text, StyleSheet, TextInput, Image, Pressable, Alert } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default function Header({navigation, route}) {
+
+  return (
+    <View style={styles.container}>
+
+      <Image style={styles.stretch} source={require('./assets/aapfolou.png')} />
+
+      <Pressable style={styles.homeBtn} onPress={ () => { navigation.navigate('Home', {idUser: 1}) } }>
+        <MaterialIcons name="home" size={24} color="black" />
+      </Pressable>
+
+      <Pressable style={styles.homeBtn} onPress={ () => { navigation.navigate('Login', {idUser: 1}) } }>
+        <MaterialIcons name="logout" size={24} color="black" />
+      </Pressable>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: "100%",
+    marginTop: 20,
+    backgroundColor: '#766452',
+  },
+  stretch: {
+    height: 40,
+    resizeMode: 'contain',
+  },
+  homeBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 10,
+    elevation: 3,
+  },
+  logoutBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 10,
+    elevation: 3,
+  },
+  logintxt: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
