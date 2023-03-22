add1f440bda0bc8a273cbabfdb3468bc6a28941b
Lucas Beluomini <lucasbeluomini1@gmail.com>
2023-03-22 12:08:21 -0300

    adicionando rotas principais

--- mobile/App.js
+++ mobile/App.js
@@ -6,6 +6,8 @@ const Stack = createNativeStackNavigator();
 
 import LoginScreen from './Login';
 import HomeScreen from './Home';
+import ProductScreen from './Product';
+import Header from './Header';
 
 const App = () => {
   return (
@@ -13,6 +15,7 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+        <Stack.Screen name="Product" component={ProductScreen} options={{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
