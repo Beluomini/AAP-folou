14c3a75a40d7024e80d78e8558cd47d9af90f476
Lucas Beluomini <lucasbeluomini1@gmail.com>
2023-03-27 08:33:18 -0300

    add pagina de cart

--- mobile/App.js
+++ mobile/App.js
@@ -7,7 +7,7 @@ const Stack = createNativeStackNavigator();
 import LoginScreen from './Login';
 import HomeScreen from './Home';
 import ProductScreen from './Product';
-import Header from './Header';
+import CartScreen from './Cart';
 
 const App = () => {
   return (
@@ -16,6 +16,7 @@ const App = () => {
         <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
         <Stack.Screen name="Product" component={ProductScreen} options={{headerShown: false}} />
+        <Stack.Screen name="Cart" component={CartScreen} options={{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
